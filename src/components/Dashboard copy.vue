80fb22a857681e1a45aa4b72feedaed580336b13
Christopher <cquirozmendivel@gmail.com>
2024-07-16 14:38:01 +0200

    Prueba

---
+++ src/components/Dashboard copy.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="dashboard">
+      <h1>Dashboard</h1>
+      <p>Contenido específico del dashboard...</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Dashboard',
+  };
+  </script>
+  
+  <style scoped>
+  .dashboard {
+  }
+  </style>
+  
